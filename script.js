45792bae71e4722f6f7862efa2c108f78d13672e
alalalisa <alalalisa@users.noreply.github.com>
2025-12-14 22:09:34 +0300

    Restore splash screen functionality and fix page title

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+// ========== КОНФИГУРАЦИЯ CLOUDINARY ==========
+const CLOUDINARY_CONFIG = {
+    CLOUD_NAME: 'dwwyducge',
+    IMAGE_BASE_URL: 'https://res.cloudinary.com/dwwyducge/image/upload/',
+    VIDEO_BASE_URL: 'https://res.cloudinary.com/dwwyducge/video/upload/'
+};
+
+// Функция для получения полного URL изображения
+function getCloudinaryImageUrl(filename) {
+    return `${CLOUDINARY_CONFIG.IMAGE_BASE_URL}${filename}`;
+}
+
+// Функция для получения полного URL видео
+function getCloudinaryVideoUrl(filename) {
+    return `${CLOUDINARY_CONFIG.VIDEO_BASE_URL}${filename}`;
+}
+
 // ========== ГЛОБАЛЬНЫЕ ПЕРЕМЕННЫЕ ==========
 let portfolioData = [];
 let icons = [];
